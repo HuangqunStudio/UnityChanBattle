025bc790007a85e587e2a8257e990c48abac6a67
Kefan Yang <starordust@gmail.com>
2019-07-18 19:30:13 -0700

    initial commit

---
+++ Assets/Scripts/FreezeXZRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeXZRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
+    }
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
