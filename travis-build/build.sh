87ef74b5667067a50ea3029ccc1c7a8ebb9b6097
Yang Zhuohan <billstark1996@gmail.com>
2019-08-06 12:13:40 +0800

    fix build file

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -9,22 +9,11 @@ else
 fi
 UNITY_BUILD_DIR=$(pwd)/builds
 OSX_LOG_FILE=$UNITY_BUILD_DIR/OSX.log
-UNITY_ACTIVATION_LOG_FILE=$UNITY_BUILD_DIR/unity.activation.log
-UNITY_RETURN_LOG_FILE=$UNITY_BUILD_DIR/unity.returnlicense.log
-
-echo "Activating Unity license"
-${UNITY_PATH} \
-    -logFile "$UNITY_ACTIVATION_LOG_FILE" \
-    -username ${UNITY_USER} \
-    -password ${UNITY_PWD} \
-    -batchmode \
-    -noUpm \
-    -quit
-echo "Unity activation log:"
-cat $UNITY_ACTIVATION_LOG_FILE
 
 echo "Attempting to build $UNITY_PROJECT_NAME for OSX"
 ${UNITY_PATH} \
+  -username ${UNITY_USER} \
+  -password ${UNITY_PWD} \
   -batchmode \
   -nographics \
   -silent-crashes \
@@ -35,15 +24,6 @@ ${UNITY_PATH} \
 rc1=$?
 echo 'OSX build logs:'
 cat $OSX_LOG_FILE
-
-echo "Returning Unity license"
-${UNITY_PATH} \
-    -logFile "$UNITY_RETURN_LOG_FILE" \
-    -batchmode \
-    -returnlicense \
-    -quit
-echo "Unity return log:"
-cat $UNITY_RETURN_LOG_FILE
 
 STATUS_CODE=$(($rc1))
 echo "Finishing with code $STATUS_CODE"
